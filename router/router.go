a7815fe8284284c4eafc09ab9f630b563e61e945
douglas <douglas.edig@outlook.com>
2024-05-20 21:58:06 -0300

    feat: add directory routes and logics

--- router/router.go
+++ router/router.go
@@ -3,12 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	//Initialize Router
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 
+	//Initializes Routes
+	initializeRoutes(router)
+
+	//run the server
 	router.Run(":8080")
 }
